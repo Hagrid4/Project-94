6631aeb8144c9f0eb0f863219c751568ecef48be
Hagrid4 <88029651+Hagrid4@users.noreply.github.com>
2022-04-01 12:59:33 +0800

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyASvObcOvBpVCT9Ro6H9DXHX4IWKKHneKc",
+    authDomain: "kwitter-project-ad782.firebaseapp.com",
+    databaseURL: "https://kwitter-project-ad782-default-rtdb.firebaseio.com",
+    projectId: "kwitter-project-ad782",
+    storageBucket: "kwitter-project-ad782.appspot.com",
+    messagingSenderId: "11623629885",
+    appId: "1:11623629885:web:be54df314e1ebaa8871654"
+  };
+  
+  // Initialize Firebase
+  firbase.initializeApp(firebaseConfig);
